06ec73ad8f12ef236577901d206dc87578b6fa19
himanshu <rathihimanshu@live.com>
2025-09-25 15:04:48 +0530

    kms and secrets

--- main.tf
+++ main.tf
@@ -13,3 +13,23 @@ resource "aws_secretsmanager_secret" "topsecret1" {
     kms_key_id  = aws_kms_key.key1.key_id
     # secret_string = "my_secret_value"
 }
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    sid    = "DenyGetSecretValue"
+    effect = "Deny"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::619512840514:root"]
+    }
+
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_secretsmanager_secret_policy" "example" {
+  secret_arn = aws_secretsmanager_secret.topsecret1.arn
+  policy     = data.aws_iam_policy_document.example.json
+}
